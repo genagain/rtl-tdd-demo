e566d6dd1c1df265950b917ca019efed5ad84cb0
Gen Ohta <myself@genohta.com>
2021-09-07 14:58:44 -0700

    test index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 function Index() {
   return (
     <div>
-      <div>Index</div>
+      <h1>Index</h1>
+      <Link to="/sign-up">Sign up</Link>
     </div>
   )
 }
