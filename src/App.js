015451364fcbfb958fefa72ea0d9d329d799ec37
Gen Ohta <myself@genohta.com>
2021-09-08 09:37:11 -0700

    add all of the fields

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Switch, Route } from 'react-router-dom'
 
 import Index from './pages/Index'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <>
       <Switch>
         <Route path="/" exact component={Index} />
+        <Route path="/sign-up" exact component={Signup} />
       </Switch>
     </>
   );
